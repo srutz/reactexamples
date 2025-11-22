edf176d992b8ea03e0654187af7b34be9e0a1132
stepan.rutz <stepan.rutz@gmx.de>
2025-11-22 14:06:23 +0100

    added recipeview

---
+++ src/RecipeView.tsx
@@ -0,0 +1,31 @@
+import type { Recipe } from "./Recipe";
+
+export function RecipeView({ recipe } : { recipe: Recipe }) {
+    return (
+        <div className="max-w-[600px] m-2 p-4 bg-neutral-100 border border-neutral-300 rounded shadow-xl animated fadeIn">
+            <p className="text-lg font-bold">{recipe.name}</p>
+            <div className="ml-4">
+            <p className="mt-2 font-semibold">Summary:</p>
+                <p>Prep Time: {recipe.prepTimeMinutes} minutes</p>
+                <p>Cook Time: {recipe.cookTimeMinutes} minutes</p>
+                <p>Servings: {recipe.servings}</p>
+                <p>Difficulty: {recipe.difficulty}</p>
+                <p>Cuisine: {recipe.cuisine}</p>
+                <p>Calories per Serving: {recipe.caloriesPerServing}</p>
+            </div>
+            <p className="mt-2 font-semibold">Ingredients:</p>
+            <ul>
+                {recipe.ingredients.map((item, index) => (
+                    <li key={index} className="ml-8 list-disc">{item}</li>
+                ))}
+            </ul>
+            <p className="mt-2 font-semibold">Instructions:</p>
+            <ul>
+                {recipe.instructions.map((item, index) => (
+                    <li key={index} className="ml-8 list-decimal">{item}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
