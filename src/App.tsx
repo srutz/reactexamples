b6c6e40a9a7f9491eec504d11d9614ad8b2b088c
stepan.rutz <stepan.rutz@gmx.de>
2025-11-22 12:18:17 +0100

    simple animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 
 export function App() {
     return (
-        <div>Hello World</div>
+        <div>
+            <Title message="Welcome to React" />
+        </div>
+    )
+}
+
+function Title({message }: {message: string}) {
+    const chars = message.split("")
+    return (
+        <div className="flex text-lg font-bold ">
+            {chars.map((char, index) => (
+                <Char char={char} index={index} key={index} />
+            ))}
+        </div>
+    )
+}
+
+function Char({char, index}: {char: string, index: number}) {
+    return (
+        <div className="mx-[-0.5px] animate-flip-up"
+            style={{ animationDelay: `${index * 50}ms` }}
+            >
+            {char === " " ? "\u00A0" : char}
+        </div>
     )
 }
