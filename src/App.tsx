f93febf9384bea69980bcedab2e3d3ba3614b7e9
stepan.rutz <stepan.rutz@gmx.de>
2025-11-22 13:49:19 +0100

    recipe with fetch

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import { fetchRecipe, type Recipe } from "./Recipe";
 import { RecipeView } from "./RecipeView";
 
 export function App() {
+    const [recipeId, setRecipeId] = useState(1);
     const [recipe, setRecipe] = useState<Recipe | null>(null);
     useEffect(() => {
         (async () => {
-            const fetchedRecipe = await fetchRecipe(11);
+            const fetchedRecipe = await fetchRecipe(recipeId);
             setRecipe(fetchedRecipe);
         })();
-    }, []);
+    }, [recipeId]);
     if (!recipe)
         return null;
     return (
-        <RecipeView recipe={recipe} />
+        <div className="w-[100vw] h-[100vh] bg-red-300 overflow-auto flex flex-col items-center">
+            <div className="flex gap-2 items-center mt-4 mb-2">
+                <button onClick={() => setRecipeId(Math.max(1, recipeId - 1))}>Prev</button>
+                <button onClick={() => setRecipeId(recipeId + 1)}>Next</button>
+            </div>
+            <RecipeView recipe={recipe} />
+        </div>
     )
 }
