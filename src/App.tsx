9d74a88044d8ee6e647de156b0e3b7bf2ceb3f13
stepan.rutz <stepan.rutz@gmx.de>
2025-11-24 14:24:48 +0100

    windowsize

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react"
 
 export function App() {
     return (
-        <div>Hello World</div>
+        <div>
+            <WindowInfo></WindowInfo>
+        </div>
+    )
+}
+
+function WindowInfo() {
+    const [val, setVal] = useState(1)
+    useEffect(() => {
+        const ls = () => {
+            setVal(-val) // trigger re-render
+        }
+        window.addEventListener("resize", ls);
+        return () => window.removeEventListener("resize", ls)
+    }, [val])
+    return (
+        <div>Windowsize {window.innerWidth} x {window.innerHeight}</div>
     )
 }
