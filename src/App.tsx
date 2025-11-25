008ee317e01e6e1bdfbc9cf55932e36440481dbe
stepan.rutz <stepan.rutz@gmx.de>
2025-11-25 15:00:39 +0100

    soft-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { RecipeView } from "./RecipeView";
 import { useRecipe } from "./hooks/useRecipe";
+import { useNavigate } from "react-router";
 
 export function App() {
+    const navigate = useNavigate();
     const [recipeId, setRecipeId] = useState(() => 5);
     const { data: recipe, isFetching: fetching, refetch } = useRecipe(recipeId)
+    const handleHelp = () => {
+        navigate("/help")
+    }
     return recipe && (
         <div className="w-[100vw] h-[100vh] bg-red-300 overflow-auto flex flex-col items-center">
             <div className="flex gap-2 items-center mt-4 mb-2">
@@ -17,6 +22,7 @@ export function App() {
                         }}>
                     Next</button>
                 <button onClick={() => refetch()}>Reload</button>
+                <button onClick={handleHelp}>Go to help</button>
             </div>
                 <RecipeView recipe={recipe} />
         </div>)
