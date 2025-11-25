3432c2ed465012f6ba7ca30d8b247fd8d3d8ea1a
stepan.rutz <stepan.rutz@gmx.de>
2025-11-25 15:34:58 +0100

    help page and recipe page

--- src/HelpPage.tsx
+++ src/HelpPage.tsx
@@ -1,15 +1,10 @@
-import { useNavigate } from "react-router"
+import { NavLink } from "react-router"
 
 export function HelpPage() {
-    const navigate = useNavigate();
-    const handleMain = () => {
-        navigate("/")
-        //location.href = "/"
-    }
     return (
         <div className="w-[100vw] h-[100vh] bg-indigo-300 flex flex-col gap-2 items-center">
             <div>Hilfe lorem ipsum</div>
-            <button onClick={handleMain}>Go to main</button>
+            <NavLink to="/">Go to main</NavLink>
         </div>
     )
 }
--- src/RecipePage.tsx
+++ src/RecipePage.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import { RecipeView } from "./RecipeView";
 import { useRecipe } from "./hooks/useRecipe";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 export function RecipePage() {
     const navigate = useNavigate();
-    const [recipeId, setRecipeId] = useState(() => 5);
+    const { id } = useParams()
+    const recipeId = id ? Number.parseInt(id) : 1
     const { data: recipe, isFetching: fetching, refetch } = useRecipe(recipeId)
     const handleHelp = () => {
         navigate("/help")
@@ -14,13 +14,11 @@ export function RecipePage() {
         <div className="w-[100vw] h-[100vh] bg-red-300 overflow-auto flex flex-col items-center">
             <div className="flex gap-2 items-center mt-4 mb-2">
                 <button disabled={fetching}
-                        onClick={() => setRecipeId(Math.max(1, recipeId - 1))}>
-                    Prev</button>
+                        onClick={() => navigate("/recipe/" + Math.max(1, recipeId - 1))
+                    }>Prev</button>
                 <button disabled={fetching}
-                        onClick={() => {
-                            setRecipeId((oldId) => oldId + 1)
-                        }}>
-                    Next</button>
+                        onClick={() => navigate("/recipe/" + (recipeId + 1))
+                    }>Next</button>
                 <button onClick={() => refetch()}>Reload</button>
                 <button onClick={handleHelp}>Go to help</button>
             </div>
