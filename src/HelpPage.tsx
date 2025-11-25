5c4b0aabbb8bbc93c630a165246a07a271d47be3
stepan.rutz <stepan.rutz@gmx.de>
2025-11-25 14:52:55 +0100

    routerbranch

---
+++ src/HelpPage.tsx
@@ -0,0 +1,8 @@
+
+export function HelpPage() {
+    return (
+        <div className="w-[100vw] h-[100vh] bg-indigo-300 flex flex-col gap-2 items-center">
+            <div>Hilfe lorem ipsum</div>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles.css"
 import { App } from './App'
+import { createBrowserRouter, RouterProvider } from "react-router"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { HelpPage } from './HelpPage'
 
 const queryClient = new QueryClient();
+const router = createBrowserRouter([
+    {
+        path: "/", element: <App/>
+    },
+    {
+        path: "/help", element: <HelpPage />
+    },
+    {
+        path: "*", element: <div>Die Seite gibt es nicht</div>
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
     </QueryClientProvider>
 )
